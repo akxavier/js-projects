9033edfb20bad20b65431a5e6dd19c99a07b3414
akxavier <77876814+akxavier@users.noreply.github.com>
2022-02-25 12:21:55 +0530

    Added camelize.js

---
+++ javascript.info/camelize.js
@@ -0,0 +1,11 @@
+function camelize(str) {
+    alert(str
+        .split('-')
+        .map( (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1) )
+        .join('')
+        );
+}
+
+camelize("background-color");
+camelize("list-style-image");
+camelize("-webkit-transition");
